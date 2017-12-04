b491a9de7c981e812c5c214d20a9785c162b8f43
Aryan Blaauw <ablaauw@gmx.net>
2017-12-04 16:10:57 +0100

    Create gpuhostconfig.sh

---
+++ gpuhostconfig.sh
@@ -0,0 +1,31 @@
+# this script configures any GPU host for vGPU CUDA Cluster 2018 usage
+# tnx go out to NV and Rancher for creating the coolest container solution on the planet
+
+# prep
+sudo add-apt-repository -y universe
+sudo add-apt-repository -y multiverse
+sudo add-apt-repository -y ppa:graphics-drivers/ppa
+sudo apt-get update
+
+# kernel upgrade
+sudo apt-get -y install linux-image-extra-$(uname -r)
+
+# fetch the newest docker 
+sudo wget -qO- https://get.docker.com/ | sh
+
+# get the NV drivers for connectivity to the PCIe layer
+sudo apt-get -y install nvidia-375 nvidia-cuda-toolkit ubuntu-desktop
+sudo apt-get -y install nvidia-modprobe
+curl -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
+sudo tee /etc/apt/sources.list.d/nvidia-docker.list <<< "deb https://nvidia.github.io/libnvidia-container/ubuntu16.04/amd64 deb https://nvidia.github.io/nvidia-container-runtime/ubuntu16.04/amd64 deb https://nvidia.github.io/nvidia-docker/ubuntu16.04/amd64 /"
+
+# fetch update, install nv-dckr2.0
+sudo apt-get update
+sudo apt-get install nvidia-docker2
+sudo pkill -SIGHUP dockerd
+
+# fetch rancher 2.0 for controlling the infra nodes ( todo: add the node[s)
+docker run -d --restart=unless-stopped -p 80:8080 rancher/server:preview
+
+# fetch zerotier client for secure access to the controller ( todo: add the client to a ZT network )
+curl -s https://install.zerotier.com/ | sudo bash
